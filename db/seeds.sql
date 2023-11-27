49077a0e0ed9c606d60946856b8e61d32fe195bd
jj <saoitjensen@gmail.com>
2023-11-27 02:43:58 -0700

    seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,7 +8,7 @@ INSERT INTO role (title, salary, department_id)
 VALUES
 ("full stack", 50000, 1),
 ("lead", 100000, 2),
-("director",120000, 3);
+("director", 120000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
